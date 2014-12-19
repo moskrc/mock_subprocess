9ec560dbb62708d5d1b8c53890bc4018ef259f86
Vitaliy Shishorin <moskrc@gmail.com>
2014-12-19 14:07:15 +0300

    works

--- src/f2.py
+++ src/f2.py
@@ -4,9 +4,12 @@ import subprocess
 def action():
     res = subprocess.Popen(['ls', '-a'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+    print res.communicate
     out, err = res.communicate()
 
     if res.returncode != 0:
         raise Exception(out)
+
+    print out
 
     return out
--- tests.py
+++ tests.py
@@ -7,25 +7,27 @@ import unittest
 
 class RmTestCase(unittest.TestCase):
 
-
-    @mock.patch('src.f1.subprocess.Popen')
-    @mock.patch('src.f2.subprocess.Popen')
+    @mock.patch('src.f1.subprocess')
+    @mock.patch('src.f2.subprocess')
     def test_superfunction(self, subprocess_mock_f2, subprocess_mock_f1):
 
-        process_mock_f1 = mock.Mock()
+        process_mock_f1 = mock.Mock(name='mock1')
         attrs1 = {'communicate.return_value': ('osx', None), 'returncode': 0}
         process_mock_f1.configure_mock(**attrs1)
-        subprocess_mock_f1.return_value = process_mock_f1
+        subprocess_mock_f1.Popen.return_value = process_mock_f1
 
-        process_mock_f2 = mock.Mock()
+        process_mock_f2 = mock.Mock(name='mock2')
         attrs2 = {'communicate.return_value': ('win', None), 'returncode': 0}
         process_mock_f2.configure_mock(**attrs2)
-        subprocess_mock_f2.return_value = process_mock_f2
+        subprocess_mock_f2.Popen.return_value = process_mock_f2
 
         res = superfunction()
 
-        self.assertEqual(res, {'data': '0 error output'})
+        self.assertEqual(res, ['osx', 'win'])
 
-        self.assertTrue(subprocess_mock_f1.called)
-        self.assertTrue(subprocess_mock_f2.called)
+        self.assertEqual(subprocess_mock_f1.Popen.call_count, 1)
+        self.assertEqual(subprocess_mock_f1.Popen.call_count, 1)
+
+        self.assertTrue(subprocess_mock_f1.Popen)
+        self.assertTrue(subprocess_mock_f2.Popen)
 
